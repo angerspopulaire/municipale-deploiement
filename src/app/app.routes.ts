83d6b6f984339053313fc974ffa967d0df84675a
Ludovic Angenard <ludovic.angenard@orange.fr>
2026-03-05 02:07:23 +0100

    v1.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,17 @@ const routeConfig: Routes = [
     {
         path: '',
         loadComponent: () => import('./accueil/accueil').then(m => m.Accueil),
-        title: 'Accueil - élection municipale 2026 - Angers populaire - France insoumise',
+        title: 'Angers populaire - élections municipales le 15 et 22 mars 2026 - France insoumise',
     },
     {
         path: 'mentions-legales',
         loadComponent: () => import('./mention-legales/mention-legales').then(m => m.MentionLegales),
-        title: 'Mentions légales - élection municipale 2026 - Angers populaire - France insoumise',
+        title: 'Mentions légales - élections municipales le 15 et 22 mars 2026 - Angers populaire - France insoumise',
     },
     {
         path: 'programme',
         loadComponent: () => import('./pogramme/pogramme').then(m => m.Pogramme),
-        title: 'Notre programme - élection municipale 2026 - Angers populaire - France insoumise',
+        title: 'Notre programme - élections municipales le 15 et 22 mars 2026 - Angers populaire - France insoumise',
     },
 ];
 export default routeConfig;
